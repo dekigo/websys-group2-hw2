c26456a7461bc5fd681fcfb9dd4f93518aed4b53
FTLspawn <macgregor.evan@gmail.com>
2013-10-15 17:30:58 -0400

    Work on HTML + JS

---
+++ hexed.js
@@ -0,0 +1,18 @@
+/* Hexed! - a jQuery plugin *
+ *                          *
+ *                          *
+ *                          */
+/*
+(function( $ ) {
+  $.fn.generateColor = function( id ) {
+    //var r = 0;
+    //var g = 0;
+    //var b = 0;
+    //var rgb = "#" + r + g + b;
+    //$(id).css("color",rgb);
+  }
+}( jQuery ));*/
+
+$(function(){
+  $( ".slider" ).slider();
+});
