e18721135c8a8e139f569f52233d206e2626fc35
FTLspawn <macgregor.evan@gmail.com>
2013-10-17 13:24:40 -0400

    Added second swatch to compare against first

--- hexed.js
+++ hexed.js
@@ -13,15 +13,21 @@
     for (var i = 0; i < 6; i++ ) {
         color += letters[Math.round(Math.random() * 15)];
     }
-    $("#swatch").css( "background-color", color );
+    $("#first_swatch").css("background-color",color);
   }
   function refreshValues() {
-    var r = $("#slider_red").slider( "value" );
-    var g = $("#slider_green").slider( "value" );
-    var b = $("#slider_blue").slider( "value" );
+    var r = $("#slider_red").slider("value");
+    var g = $("#slider_green").slider("value");
+    var b = $("#slider_blue").slider("value");
     $("#red_in").attr("value",r);
     $("#green_in").attr("value",g);
     $("#blue_in").attr("value",b);
+    var red = r.toString(16), green = g.toString(16), blue = b.toString(16);
+    if (red.length == 1) { red = "0" + red; }
+    if (green.length == 1) { green = "0" + green; }
+    if (blue.length == 1) { blue = "0" + blue; }
+    var color = "#" + red.toUpperCase() + green.toUpperCase() + blue.toUpperCase();
+    $("#second_swatch").css("background-color",color);
   }
   $(function(){
     $(".slider").slider({
